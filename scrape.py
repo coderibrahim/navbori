7f4ce995b00f6a30735a1279183b7febe462c6f9
coderibrahim <bekirakbas41@gmail.com>
2023-07-08 15:53:48 +0300

    app now updated

--- scrape.py
+++ scrape.py
@@ -11,18 +11,15 @@ import cloudscraper
 class Functionalities:
     def scrape_website(self, barcode_number):
         search_url = 'https://marketkarsilastir.com/ara/' + barcode_number
-        scraper = cloudscraper.create_scraper(disableCloudflareV1=True)
-        #scraper = cloudscraper.create_scraper(delay=10, browser='chrome')
-
-        response = scraper.get(search_url)
 
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
-        #response = requests.get(search_url)
+        response = requests.get(search_url)
         #response = requests.get(search_url, headers=headers)
-
-        soup = BeautifulSoup(response.content, 'html.parser')
+        scraper = cloudscraper.create_scraper(delay=10, browser='chrome')
+        response = scraper.get(search_url).text
+        soup = BeautifulSoup(response, 'html.parser')
 
         print("soup data:")
         print(soup.prettify())
